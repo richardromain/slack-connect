66d301c3f67e4dc943fa27985c93bb57b48a6375
Romain Richard <romain.richard.19@gmail.com>
2017-03-29 11:37:33 +0200

    Display last messages on home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Providers\SlackServiceProvider;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $slack = new SlackServiceProvider();
+        $messages = $slack->getChannelMessages('C09C27RAT', 5);
+
+        return view('home', compact('messages'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,19 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
+            <div class="list-group">
+                @foreach($messages->messages as $message)
+                    <div class="list-group-item">
+                        <h4 class="list-group-item-heading">
+                            @if(property_exists($message, 'user'))
+                                {{ $message->user }}
+                            @else
+                                {{ $message->username }}
+                            @endif
+                        </h4>
+                        <p class="list-group-item-text">{{ $message->text }}</p>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
